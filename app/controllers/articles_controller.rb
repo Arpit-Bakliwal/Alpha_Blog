1221624ed8ed537697f88cc917d4a77c8530c721
arpitbatmango <arpitb.hub@hubmail.mangoapps.com>
2022-09-23 15:57:41 +0530

    Use controller method as helper method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article]
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         # render plain: @article.inspect
         if @article.save
             flash[:notice] = "Article created succesfully."
