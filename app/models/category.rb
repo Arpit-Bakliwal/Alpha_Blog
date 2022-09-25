b69ccacc06d55fb4b6141d336fae6054963fb745
arpitbatmango <arpitb.hub@hubmail.mangoapps.com>
2022-09-25 14:43:44 +0530

    Add category model validation for uniqueness with alternate approach

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
-    validates :name, presesence: true, length: { minimum: 3, maximum: 25 }
-    validates_uniqueness_of :name
+    validates :name, presence: true, 
+                     uniqueness: { case_sensitive: false }, 
+                     length: { minimum: 3, maximum: 25 }
+    # validates_uniqueness_of :name
 end
